3739dfd33d7e85d1de133561ab20ede6d6561498
Breno Gianotto <breno.gntt@gmail.com>
2020-03-28 16:35:26 -0300

    Seting up startEditExpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -42,6 +42,16 @@ export const editExpense = (id, updates) => ({
   updates
 });
 
+export const startEditExpense = (id, updates) => {
+  return (dispatch) => {
+    return database.ref(`expenses/${id}`).update({ // return da promise do database - retorno pra quem chamou 
+      ...updates
+    }).then(() => {
+      dispatch(editExpense(id, updates));
+    })
+  }
+}
+
 // SET_EXPENSES
 
 export const setExpenses = (expenses) => ({
